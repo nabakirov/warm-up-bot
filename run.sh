eb23fe4d9829e569326abeb12bbd17845de09acb
Nurs <nabakirov@gmail.com>
2019-07-29 13:18:59 +0600

    changed chat id

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source /.env && \
 curl --request GET \
-  --url "https://api.telegram.org/bot{$TOKEN}/sendMessage?chat_id=285268345&text=%D1%80%D0%B0%D0%B7%D0%BC%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%BC%D0%B0%D0%B7%D0%B0%D1%84%D0%B0%D0%BA%D0%B0"
+  --url 'https://api.telegram.org/bot891609049:AAGjF7gEp6fRWQ9PDAN1cp_Shdusj76NiLQ/sendMessage?chat_id=-271519707&text=%D1%80%D0%B0%D0%B7%D0%BC%D0%B8%D0%BD%D0%BA%D0%B0'
